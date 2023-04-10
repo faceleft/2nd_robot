2ec6ca48c0d9c222b6e18fe94a7e2ae44726926a
faceleft <89391656+faceleft@users.noreply.github.com>
2023-04-10 20:18:16 +0700

    Update 2nd_robot.ino

--- 2nd_robot.ino
+++ 2nd_robot.ino
@@ -12,7 +12,7 @@ void loop()
 {
   if (dm::changeFlag) {
     dm::changeFlag = false;
-    //Serial.print("Sensors: ");
+    Serial.print("Sensors: ");
     for (int i = 0; i < dm::SENSORS_NUM; i++) {
       Serial.print(dm::pulseTimeUs[i]);
       Serial.print(" ");
